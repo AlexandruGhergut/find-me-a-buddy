d21d6159a0fa2144e9a2fe50257e68813c3bdaea
alexandru <alexandru.ghergut94@gmail.com>
2018-11-06 22:01:08 +0200

    Initial commit

---
+++ src/main/java/com/fmi/findmeabuddy/FindMeABuddyApplication.java
@@ -0,0 +1,12 @@
+package com.fmi.findmeabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeABuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMeABuddyApplication.class, args);
+	}
+}
