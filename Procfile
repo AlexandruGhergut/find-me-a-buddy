75e9dac72f049fd8ec0ff74db8016c4ef930bc03
AlexandruGhergut <alexandru.ghergut94@gmail.com>
2018-11-19 10:55:48 +0200

    Adjust app for AWS Beanstalk environment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/find-me-a-buddy-1.0.0.jar
--- src/main/java/com/fmi/findmeabuddy/controller/HelloWorldController.java
+++ src/main/java/com/fmi/findmeabuddy/controller/HelloWorldController.java
@@ -2,12 +2,11 @@ package com.fmi.findmeabuddy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    Mono<String> get() {
-        return Mono.just("Hello world!");
+    public String get() {
+        return "Hello world";
     }
 }
