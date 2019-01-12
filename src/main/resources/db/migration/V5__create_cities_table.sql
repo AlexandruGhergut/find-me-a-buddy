9b18cd9ddbbd0055cca140452683f7c9cd81988b
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2019-01-12 21:48:58 +0200

    Add files via upload

--- src/main/resources/db/migration/V5__create_cities_table.sql
+++ src/main/resources/db/migration/V5__create_cities_table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `Cities` (
     `id` int(11) AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
+    `region_id` int(11) NOT NULL,
     `country_id` smallint(5) NOT NULL,
     `latitude` decimal(10,8) NOT NULL,
     `longitude` decimal(11,8) NOT NULL,
+    `name` varchar(255) NOT NULL,
     PRIMARY KEY(`id`)
 );
