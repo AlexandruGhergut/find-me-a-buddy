6555d8366acb7f3b7389afe2069344aed33068e0
AlexandruGhergut <alexandru.ghergut94@gmail.com>
2019-01-26 19:31:15 +0200

    Fix migrations no longer working under mysql flavor

--- src/main/resources/db/migration/V10__hobby_profile_join_table.sql
+++ src/main/resources/db/migration/V10__hobby_profile_join_table.sql
@@ -10,8 +10,17 @@ ADD CONSTRAINT `fk_ProfileHobby_Hobby` FOREIGN KEY (`hobby_id`) REFERENCES `Hobb
 ALTER TABLE `ProfileHobby`
 ADD CONSTRAINT `fk_ProfileHobby_Profile` FOREIGN KEY (`profile_id`) REFERENCES `Profile` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- Dropping multiple columns differs across SQL flavors so we'll drop one column per statement
 ALTER TABLE `Profile`
-DROP COLUMN `hobby_one`, `hobby_two`, `hobby_three`, `hobby_four`, `hobby_five`;
+DROP COLUMN `hobby_one`;
+ALTER TABLE `Profile`
+DROP COLUMN `hobby_two`;
+ALTER TABLE `Profile`
+DROP COLUMN `hobby_three`;
+ALTER TABLE `Profile`
+DROP COLUMN `hobby_four`;
+ALTER TABLE `Profile`
+DROP COLUMN `hobby_five`;
 
 ALTER Table `Profile`
 MODIFY `city_id` int(11) NOT NULL;
